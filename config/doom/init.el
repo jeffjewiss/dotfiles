d358ae0b376cebb899105da77e3df2ec46b1fa39
Jeff Jewiss <jeff@jeffjewiss.com>
2018-05-29 00:35:00 +0100

    dark/transparent titlebar for emacs

--- config/doom/init.el
+++ config/doom/init.el
@@ -132,3 +132,6 @@
 
 ;; Alchemist
 (setq alchemist-hooks-compile-on-save t)
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
