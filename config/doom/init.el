180ea8ddf9bb8eb18ca4b21f8212c75d1956b710
Jeff Jewiss <jeff@jeffjewiss.com>
2019-12-10 15:15:44 -0500

    enable java in emacs

--- config/doom/init.el
+++ config/doom/init.el
@@ -98,7 +98,7 @@
        go                ; the hipster dialect
       ;(haskell +intero) ; a language that's lazier than I am
       ;hy                ; readability of scheme w/ speed of python
-      ;(java +meghanada) ; the poster child for carpal tunnel syndrome
+       (java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
       ;julia             ; a better, faster MATLAB
       ;latex             ; writing papers in Emacs has never been so fun
