518ce1647a8503813a936f891b0495d56aa54371
Jeff Jewiss <jeff@jeffjewiss.com>
2019-12-10 15:15:22 -0500

    ignore map files for magit todos

--- config/doom/init.el
+++ config/doom/init.el
@@ -162,8 +162,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.map")))))
-)
+ '(safe-local-variable-values (quote ((magit-todos-exclude-globs "*.map")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
