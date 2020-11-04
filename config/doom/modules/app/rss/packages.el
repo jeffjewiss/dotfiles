2b6f318ddc30f684395c1f7781321afb892bd306
Jeff Jewiss <jeff@jeffjewiss.com>
2020-11-04 18:57:22 -0500

    overwrite rss app module

---
+++ config/doom/modules/app/rss/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; app/rss/packages.el
+
+(package! elfeed :pin "7b2b6fadaa498fef2ba212a50da4a8afa2a5d305")
+(package! elfeed-org :pin "77b6bbf222487809813de260447d31c4c59902c9")
