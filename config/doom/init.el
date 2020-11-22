aa6320e8d43379148d3593bc1e4a14f8662d2f5f
Jeff Jewiss <jeff@jeffjewiss.com>
2020-11-22 01:32:06 -0500

    lsp all the things

--- config/doom/init.el
+++ config/doom/init.el
@@ -109,7 +109,7 @@
          (+dictionary    ; Enable word definition and thesaurus lookup functionality
            +offline)     ; Install and prefer offline dictionary/thesaurus
         +docsets)        ; Enable integration with Dash.app docsets
-       lsp
+       (lsp +peek)       ; auto completion and language server
        make              ; run make tasks from Emacs
        (magit +forge)    ; It's Magit! A Git porcelain inside Emacs
        pdf               ; pdf enhancements
@@ -130,7 +130,7 @@
        csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        erlang            ; an elegant language for a more civilized age
-       elixir            ; erlang done right
+       (elixir +lsp)     ; erlang done right
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ess               ; emacs speaks statistics
@@ -138,7 +138,7 @@
       ;(haskell +intero) ; a language that's lazier than I am
       ;hy                ; readability of scheme w/ speed of python
       ;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp) ; all(hope(abandon(ye(who(enter(here))))))
       ;julia             ; a better, faster MATLAB
       ;latex             ; writing papers in Emacs has never been so fun
        ledger            ; an accounting system in Emacs
@@ -166,10 +166,10 @@
        (ruby +rails +lsp); 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
       ;scala             ; java, but good
-       sh                ; she sells (ba|z)sh shells on the C xor
-       swift             ; who asked for emoji variables?
-       web               ; the tubes
-       yaml              ; JSON, but readable
+       (sh +lsp)         ; she sells (ba|z)sh shells on the C xor
+       (swift +lsp)      ; who asked for emoji variables?
+       (web +lsp)        ; the tubes
+       (yaml +lsp)       ; JSON, but readable
 
        :email
        (mu4e +org)
@@ -180,12 +180,10 @@
       ;calendar
       ;irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
+      ;twitter           ; twitter client
 
        :config
-       ;; The default module set reasonable defaults for Emacs. It also provides
-       ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
-       ;; and additional ex commands for evil-mode. Use it as a reference for
-       ;; your own modules.
+      ;literate
        (default +bindings +smartparens))
 
 (custom-set-variables
