03f1e9ee14fb3f70bec99e08ba8ff230b59bc350
Jeff Jewiss <jeff@jeffjewiss.com>
2020-12-09 15:10:06 -0500

    add svg screenshot func to emacs

--- config/doom/config.el
+++ config/doom/config.el
@@ -107,6 +107,18 @@
   auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
   )
 
+;; https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 ;; Email
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu/mu4e")
 (setq +mu4e-backend 'offlineimap)
