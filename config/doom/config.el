b6f6f77c6ae8867a9e010c0df5d04d7fce9dcd52
Jeff Jewiss <jeff@jeffjewiss.com>
2022-02-22 23:55:49 -0500

    frame title format

--- config/doom/config.el
+++ config/doom/config.el
@@ -48,6 +48,19 @@
 
 (setq display-line-numbers-type 'visual)
 
+(setq frame-title-format
+      '(""
+        (:eval
+         (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+             (replace-regexp-in-string
+              ".*/[0-9]*-?" "☰ "
+              (subst-char-in-string ?_ ?  buffer-file-name))
+           "%b"))
+        (:eval
+         (let ((project-name (projectile-project-name)))
+           (unless (string= "-" project-name)
+             (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
+
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
 (load-theme 'doom-dark+ t)
