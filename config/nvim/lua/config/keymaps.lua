552503976bbf8ab5d6a4dd84ead1d12c8ba92eec
Jeff Jewiss <jeff@jeffjewiss.com>
2023-12-27 23:12:40 -0500

    add some initial custom nvim keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,27 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>fs", "<cmd>w<cr><esc>", { desc = "File save" })
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
+local function nmap(comb, cmd, desc)
+  vim.api.nvim_set_keymap("n", comb, cmd, { noremap = true, silent = true, desc = desc })
+end
+
+keymap("n", "<leader>fs", "<cmd>w<cr><esc>", { desc = "File save" })
+
+-- Smooth scrolling
+nmap("n", "<C-d>", "<C-d>zz")
+nmap("n", "<C-u>", "<C-u>zz")
+vim.o.scrolloff = 8
 
 -- Move to window using the <ctrl> hjkl keys
 vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Go to left window" })
@@ -15,3 +35,7 @@ vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = "Window close" })
 -- Neogit
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
 vim.keymap.set("n", "<leader>gs", "<cmd>Neogit kind=auto<cr>", { desc = "Neogit status" })
+
+--  Searching files
+nmap("<C-p>", ':lua require"telescope.builtin".git_files{use_git_root=false} <CR>', "Search git files")
+nmap("<C-f>", "<CMD> Telescope live_grep <CR>", "Grep through files")
