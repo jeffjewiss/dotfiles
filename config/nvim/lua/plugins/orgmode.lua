e2ba5aaa6fd9dbc2153e52d4cebc85e1d5879948
Jeff Jewiss <jjewiss@venacorp.com>
2023-12-28 21:28:26 -0500

    fix nvim orgmode

--- config/nvim/lua/plugins/orgmode.lua
+++ config/nvim/lua/plugins/orgmode.lua
@@ -4,24 +4,48 @@ return {
     dependencies = {
       { "nvim-treesitter/nvim-treesitter", lazy = true },
     },
-    event = "VeryLazy",
+    event = { "LazyFile", "VeryLazy" },
     config = function()
       -- Load treesitter grammar for org
       require("orgmode").setup_ts_grammar()
-
-      -- Setup treesitter
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { "org" },
-        },
-        ensure_installed = { "org" },
-      })
 
       -- Setup orgmode
       require("orgmode").setup({
         org_agenda_files = "~/org/**/*",
         org_default_notes_file = "~/org/refile.org",
+      })
+
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { "org" },
+        },
+        ensure_installed = { "org" }, -- Or run :TSUpdate org
+      })
+    end,
+  },
+  {
+    "akinsho/org-bullets.nvim",
+    config = function()
+      require("org-bullets").setup({
+        concealcursor = true,
+        symbols = {
+          checkboxes = {
+            -- done = { "✓", "OrgDone" },
+            -- todo = { " ", "OrgTODO" },
+          },
+        },
+      })
+    end,
+    lazy = false,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("cmp").setup({
+        sources = {
+          { name = "orgmode" },
+        },
       })
     end,
   },
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = "VeryLazy",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, { "markdown" })
