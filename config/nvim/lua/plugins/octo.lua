566f9321e869abcff2f7f2a5e2b9d3fd99843b7a
Jeff Jewiss <jjewiss@venacorp.com>
2023-12-28 21:27:28 -0500

    setup top level keymap for octo

--- config/nvim/lua/plugins/octo.lua
+++ config/nvim/lua/plugins/octo.lua
@@ -1,14 +1,20 @@
 return {
-  -- add symbols-outline
   {
     "pwntester/octo.nvim",
+    cmd = "Octo",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("octo").setup()
+      require("octo").setup({
+        enable_builtin = true,
+      })
+      vim.treesitter.language.register("markdown", "octo")
     end,
+    keys = {
+      { "<leader>go", "<cmd>Octo<cr>", desc = "Octo" },
+    },
   },
 }
