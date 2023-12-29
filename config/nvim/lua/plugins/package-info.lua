1b5a1e2153e020c1508da14dfc65f86765884ec6
Jeff Jewiss <jjewiss@venacorp.com>
2023-12-28 21:29:09 -0500

    fix nvim package-info

--- config/nvim/lua/plugins/package-info.lua
+++ config/nvim/lua/plugins/package-info.lua
@@ -5,5 +5,28 @@ return {
     dependencies = {
       "MunifTanjim/nui.nvim",
     },
+    config = function()
+      local wk = require("which-key")
+
+      require("package-info").setup()
+
+      wk.register({
+        p = {
+          name = "Package Info",
+        },
+      }, { prefix = "<leader>c" })
+    end,
+    keys = {
+      {
+        "<leader>cps",
+        "<cmd>lua require('package-info').show()<cr>",
+        desc = "Show package version",
+      },
+      {
+        "<leader>cph",
+        "<cmd>lua require('package-info').hide()<cr>",
+        desc = "Hide package version",
+      },
+    },
   },
 }
