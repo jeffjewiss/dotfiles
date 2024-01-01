0ea40957757a215ad64734abc570dc8eb14a3009
Jeff Jewiss <jeff@jeffjewiss.com>
2024-01-01 10:45:31 -0500

    update neo-test config

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,14 +1,34 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "marilari88/neotest-vitest",
+      "haydenmeade/neotest-jest",
+      "thenbe/neotest-playwright",
+      "jfpedroza/neotest-elixir",
+      "nvim-neotest/neotest-vim-test",
+    },
     opts = {
       adapters = {
+        "neotest-vitest",
+        "neotest-playwright",
+        ["neotest-jest"] = {
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        },
         ["neotest-elixir"] = {
           extra_formatters = { "ExUnit.CLIFormatter", "ExUnitNotifier" },
         },
-        "neotest-jest",
-        "neotest-playwright",
+        ["neotest-vim-test"] = {
+          ignore_filetypes = { "elixir", "javascript", "typescript" },
+        },
       },
     },
   },
