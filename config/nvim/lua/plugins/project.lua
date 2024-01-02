7807b8b055e954df0e0835b19c6330bcbae847e9
Jeff Jewiss <jeff@jeffjewiss.com>
2024-01-02 08:27:36 -0500

    try different project.nvim config

--- config/nvim/lua/plugins/project.lua
+++ config/nvim/lua/plugins/project.lua
@@ -3,7 +3,17 @@ return {
   {
     "ahmedkhalf/project.nvim",
     opts = {
-      manual_mode = false,
+      manual_mode = true,
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      require("lazyvim.util").on_load("telescope.nvim", function()
+        require("telescope").load_extension("projects")
+      end)
+    end,
+    keys = {
+      { "<leader>pp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
     },
   },
 }
